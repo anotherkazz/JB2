d508673469d34831a9e6a8043d0d6c13a3228526
anotherkazz <kazz.OHNO@gmail.com>
2022-09-12 17:19:17 +0900

    v-bind

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 'use strict'
 
+// ディレクティブとは、
+// v-で始まる特別な属性のこと
 
+// directivve（指令）という名前の通り、Vue.jsに
+// 何らかの指示を行う仕組みのこと
+
+// v-bind
+// v-if
+// v-show
+// v-for
+// v-on
+// v-model
 
 {
 
@@ -14,25 +25,14 @@
   // vue.mount('#basic-event')
 
   const vue = Vue.createApp({
-    data: function() {
-      return {
-        message: 'Hello Vue.js!'  ,
-        // ,区切りで複数のプロパティーが使える
-        count: 99,
-        // オブジェクトも入れられる
-        // オブジェクトはプロパティの集合です。プロパティとは名前（キー）と値（バリュー）が対になったものです。
-        user: {
-          lastName: 'Nakamura',
-          firstName: 'Yuta',
-          prefecture: 'Tokyo'
-        },
-        colors: ['Red', 'Green', 'Blue']
-      }
-    }
+    data: () =>  ({
+      message: 'Hello Vue.js'
+      
+    })
   })
   vue.mount('#basic-event')
 
-  // Cromeのデベロッパーツールのコンソールを利用してエラーメッセージを読む
+  // アロー関数に()はいるのか？
 
 
 // thisは利用できない
