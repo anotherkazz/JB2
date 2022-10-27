d8df47adf304e9375782a67ce70f0c40260cfc22
anotherkazz <kazz.OHNO@gmail.com>
2022-10-27 15:20:40 +0900

    ToDoスタート

--- js/main.js
+++ js/main.js
@@ -3,56 +3,23 @@
 
 {
 
-  // const vue = Vue.createApp({
-  //   data: () => {
-  //     return {
-  //       message: 'Hello Vue.js!'
-  //     }
-  //   }
-  // })
-  // vue.mount('#basic-event')
-
-  // 双方向データバインディング
+ 
 
   const vue = Vue.createApp({
     data: () => ({
-      newItem: '',
-      todos: []
+     
     }),
     // ボダンが押されたときのイベントハンドラ　メソッド
     methods: {
-      addItem: function(event) {
-      // console.log('Clicked!')
-      if(this.newItem === '') return
-      // 入力が空文字だったらそこで、処理を中断する
-      let todo = {
-        item: this.newItem,
-        isDone: false
       
-      }
-      this.todos.push(todo)
-      this.newItem = ''
-      // ボタンを押した後、テキストをクリアさせる。
-    }
-  }
+      
+      
+    
+ 
+
+ }  
   })
  
   vue.mount('#basic-event')
 
-  // アロー関数に()はいるのか？
-
-
-// thisは利用できない
-
-  // Vue.createApp({
-  //   data() {
-  //     return {
-  //       counter: 0
-  //     }
-  //   }
-  // }).mount('#basic-event')
-  // メソッドチェーン　こういう書き方もある。関数式であるかどうかは関係ない
-////
-}
-
-// リアクティブとは、すべての要素に対して反応的に作成すること
+}S 
