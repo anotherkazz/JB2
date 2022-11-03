4bcb66fa1db32b28d2c8860ca119afb087c16925
anotherkazz <kazz.OHNO@gmail.com>
2022-11-03 13:55:57 +0900

    一覧表示の機能実装

--- js/main.js
+++ js/main.js
@@ -9,7 +9,24 @@
     data: () => ({
       statusItems: ['未着手', '着手', '完了'], 
       filterItems: ['すべて表示', '未着手','着手', '完了'], 
-      sortItems: ['昇順', '降順'] 
+      sortItems: ['昇順', '降順'] ,
+      todos: [
+        {
+          title: 'test',
+          status: '未着手',
+          id: 1
+        },
+        {
+          title: 'test2',
+          status: '着手',
+          id: 2
+        },
+        {
+          title: 'test3',
+          status: '完了',
+          id: 3
+        }
+      ]
      
     }),
     // ボダンが押されたときのイベントハンドラ　メソッド
