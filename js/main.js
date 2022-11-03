1d8769080f168552770682ac81d9cbd662679dd3
anotherkazz <kazz.OHNO@gmail.com>
2022-11-03 17:38:36 +0900

    ToDo完成

--- js/main.js
+++ js/main.js
@@ -8,8 +8,11 @@
   const vue = Vue.createApp({
     data: () => ({
       statusItems: ['未着手', '着手', '完了'], 
-      filterItems: ['すべて表示', '未着手','着手', '完了'], 
+      filterItems: ['全て表示', '未着手','着手', '完了'], 
       sortItems: ['昇順', '降順'] ,
+      selectedFilterItem: '全て表示',
+
+      selectedSortItem: '昇順',
       todos: [
         {
           title: 'test',
@@ -32,8 +35,12 @@
         status: '未着手',
         id: 4
       },
-      isShowEditForm: false     
-      
+      isShowEditForm: false,     
+      editedTodo: {
+        title: '',
+        status: '未着手',
+        id: 0
+      }
      
     }),
     // ボダンが押されたときのイベントハンドラ　メソッド
@@ -56,11 +63,37 @@
       },
       // 問題3. 編集フォームを非表示するように実装しよう
       updateTodo() {
+        const index = this.todos.findIndex(
+          (todo) => todo.id === this.editedTodo.id
+        )
+        this.todos[index] = this.editedTodo
         this.isShowEditForm = false
-      }            
+      } 
+      
+      
       
 
-    }  
+    },
+    computed: {
+      sortedTodos() {
+        if (this.selectedSortItem === '昇順') {
+          return this.todos.sort((a, b) => a.id - b.id)
+        } else {
+          return this.todos.sort((a, b) => b.id - a.id)
+        }
+      },
+      filteredTodos() {
+        if (this.selectedFilterItem === '全て表示') {
+          return this.sortedTodos
+        } else {
+          return this.sortedTodos.filter(
+            (todo) => todo.status === this.selectedFilterItem
+          )
+        }
+      }
+
+    } 
+
   })
  
   vue.mount('#basic-event')
